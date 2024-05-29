a111adf02f78fc91881f709274de58beea87b96a
Sheila Pietila <sheila.pietila@tutanota.com>
2024-05-29 17:13:04 -0500

    Started organized unit testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,67 @@ import Navbar from './navbar.js';
 import Home from './pages/home.js'; 
 import { HashRouter } from 'react-router-dom';
 
+// TODO finish test suites
+
+// ---- Pass/Fail: First layer. ----
+
+// Simple-path (white box test all options)
+
+test('renders site home nav button', () => {
+  render(<HashRouter><App/></HashRouter>); 
+  const buttonElement = screen.getByText(/site name/i); 
+  expect(buttonElement).toBeInTheDocument(); 
+})
+
+test('renders site meal planner nav button', () => {
+  render(<HashRouter><App/></HashRouter>); 
+  const buttonElement = screen.getByText(/meal planner/i); 
+  expect(buttonElement).toBeInTheDocument(); 
+})
+
+test('renders site example page nav button', () => {
+  render(<HashRouter><App/></HashRouter>); 
+  const buttonElement = screen.getByText(/react example/i); 
+  expect(buttonElement).toBeInTheDocument(); 
+})
+
+test('renders site about nav button', () => {
+  render(<HashRouter><App/></HashRouter>); 
+  const buttonElement = screen.getByText(/about/i); 
+  expect(buttonElement).toBeInTheDocument(); 
+})
+
+// Code-path (non-basic method calls: N/A?) 
+
+// Parameter-range (args and results: N/A, no args)
+
+
+
+
+
+
+
+
+
+// Performance: Slideable goal, "better" and "worse", involves speed and memory. 
+//    How To?: https://stackoverflow.com/questions/62214918/how-to-performance-test-react-js-application
+// Stress-Test: Testing worst case performance loads. 
+//    How To?: See above
+// Simulation: Connection with outside factors (e.g. slow internet, internet breaks at various levels). 
+//    How To?: (Currently using with the router wrapper, what else?)
+
+// Presentation layer: Is MVC being written correctly : In React, is Flux store-view-dispatcher being written correctly. 
+//    How To?: 
+
+// Data Transaction: For testing less-than-database stored value interaction. N/A
+// Collection management: For testing database interaction. N/A
+// Multithreading: For testing multithreading, if applicable. N/A
+
+
+
+
+/* Old, practice tests
+
 test('renders learn react link', () => {
   render(<Example />);
   const linkElement = screen.getByText(/learn react/i);
@@ -35,3 +96,5 @@ describe('Example', () => {
     //screen.debug(); 
   }); 
 });
+
+*/
