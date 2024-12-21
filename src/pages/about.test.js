b61d3ee586aaeaae806e9cf9803628e4d7f8c699
Sheila Pietila <sheila.pietila@tutanota.com>
2024-12-20 18:40:12 -0600

    Outlined plans via test todos

---
+++ src/pages/about.test.js
@@ -0,0 +1,8 @@
+import about from './about.js';
+
+describe('About page', () => {
+  it.todo('renders a picture - assumed to be photo of author')
+  it.todo('has accessibility text on the photo')
+  it.todo('has a text block with minimum 50 characters - assumed to be elevator pitch')
+})
+
